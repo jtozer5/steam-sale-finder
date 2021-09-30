ef8f24d5930e2a8810cf5bc1b9cf43673fd72662
Josh Tozer <joshiantozer@gmail.com>
2021-09-29 21:20:54 -0700

    Added Readme, updated api key, cleaned up

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # game-sale-finder
+
+* This is a script I created for a python class in 2019. It was working at one point, but I'm having an issue with the steamapi now.  The code is still pretty clear and works.
+
+* This script is designed to take your Steam user name, check the genres of the most recently played games and cross reference those genres with games currently on sale.
+
+* There are a couple of options for delivery built in.  Commented sections have the code for running a scheduler in a separate thread so that the script can email the results to a user every week.
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+def local_requirements():
+    req_list = []
+    with open('requirements.txt') as requirements_file:
+        req_list = [line.strip() for line in requirements_file.readlines()]
+    install_reqs = list(filter(None, req_list))
+    return install_reqs
+
+
+setup(name='steamapi',
+      version='0.1',
+      description='An object-oriented Python 2.7+ library for accessing the Steam Web API',
+      url='https://github.com/smiley/steamapi',
+      author='Smiley',
+      author_email='',
+      license='MIT',
+      packages=['steamapi'],
+      install_requires=local_requirements(),
+      zip_safe=False)
